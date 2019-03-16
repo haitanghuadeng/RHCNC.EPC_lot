9c66a4a84f73b71a0af77dacd9bb176dc117a9af
With the light, as low as dust <43346455+haitanghuadeng@users.noreply.github.com>
2019-03-16 16:17:55 +0800

    Update EMQ_monitoring.py

--- EPC_lot/EMQ_monitoring.py
+++ EPC_lot/EMQ_monitoring.py
@@ -2,15 +2,16 @@
 
 
 import paramiko
-import EPC_lot.auto_read_conf as epc
+import auto_read_conf as epc
 
 
 class EpcSsh:
 
     def __init__(self):
-
         self.client = paramiko.SSHClient()
         self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self.colony_frame_auto = epc.Colony_Frame_Auto()
+        self.colony_frame_auto.colony_frame_fb(fb='EPC-simple.conf')
 
     def epc_ssh(
             self,
@@ -24,8 +25,9 @@ class EpcSsh:
         :param port: 默认端口为22(ssh)
         :param username: 默认为空
         :param password: 默认为空
-        :param user_info: 默认为None, 如果为True则保持连接
-        :return:
+        :param user_info: 默认为None, 如果为True则保持连接。
+            根据user_info所存储的类型进而回调，多指令请存放到{}，None则默认使用'emqx_ctl status'(不支持emqtt2.x版本)
+        :return: 单命令回弹，{}则返回每条命令所执行的结果，以键值对方式返回
         """
 
         self.client.connect(hostname, port, username, password)
@@ -37,7 +39,6 @@ class EpcSsh:
                 for i in user_info:
                     stdin, stdout, stderr = self.client.exec_command(i)
                     result = stdout.read().decode('utf-8')
-                    print(result)
                     command_info[i] = result
 
                 return command_info
@@ -45,9 +46,6 @@ class EpcSsh:
             else:
                 while True:
                     user_info = input('>>> ')
-                    # if user_info is 'q' or 'quit':
-                    #     break
-
                     stdin, stdout, stderr = self.client.exec_command(user_info)
                     result = stdout.read().decode('utf-8')
                     print(result)
@@ -58,22 +56,9 @@ class EpcSsh:
             return result
 
     def conf_read(self):
-        colony_frame_auto = epc.Colony_Frame_Auto()
-        colony_frame_auto.colony_frame_fb(fb='EPC-simple.conf')
-        list_body = colony_frame_auto.colony_frame_slave('PERCEPTION_FRAME')
+        list_body = self.colony_frame_auto.colony_frame_slave('PERCEPTION_FRAME')
         return list_body['info_server_ip']
 
     def epc_close(self):
 
         self.client.close()
-
-
-if __name__ == '__main__':
-    epcssh = EpcSsh()
-    ip = epcssh.conf_read()
-    epcssh.epc_ssh(
-        hostname=ip,
-        username=None,
-        password=None,
-        user_info=True)
-    epcssh.epc_close()
